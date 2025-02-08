3002c6fc91fde3739259337b9034afce52678ec5
sahilbisht <bishtrock890@gmail.com>
2025-02-08 15:31:02 +0530

    chat in different sections

--- public/script.js
+++ public/script.js
@@ -1,4 +1,40 @@
 
+// console.log('script.js loaded');
+// const socket = io();
+
+// function sendMessage(user) {
+//     let input = document.getElementById(`input-${user}`);
+//     let message = input.value.trim();
+//     if (message === "") return;
+//     socket.emit('chat message',{ user , message});
+//     input.value = "";
+// }
+
+// socket.on("chat message", ({user , message}) => {
+//     const reciever = user === 1 ? 2 : 1;
+
+//     // to create at reciver side
+//     let chat = document.getElementById(`chat-box-${reciever}`);
+//     let newMessage = document.createElement('div');
+//     newMessage.classList.add('reciever','message')
+//     newMessage.textContent = `${message}`;
+//     chat.appendChild(newMessage);
+//     newMessage.scrollIntoView({ behavior: "smooth", block: "end" });
+
+
+
+//     // to create at sender side
+//     let senderChat = document.getElementById(`chat-box-${user}`);
+//     let senderMessage = document.createElement('div');
+//     senderMessage.classList.add('sender','message')
+//     senderMessage.textContent = `${message}`;
+//     senderChat.appendChild(senderMessage);
+//     senderMessage.scrollIntoView({ behavior: "smooth", block: "end" });
+
+// });
+
+
+
 console.log('script.js loaded');
 const socket = io();
 
@@ -6,29 +42,26 @@ function sendMessage(user) {
     let input = document.getElementById(`input-${user}`);
     let message = input.value.trim();
     if (message === "") return;
-    socket.emit('chat message',{ user , message});
-    input.value = "";
-}
+    socket.emit('chat message',message);
 
-socket.on("chat message", ({user , message}) => {
-    const reciever = user === 1 ? 2 : 1;
-
-    // to create at reciver side
-    let chat = document.getElementById(`chat-box-${reciever}`);
-    let newMessage = document.createElement('div');
-    newMessage.classList.add('reciever','message')
-    newMessage.textContent = `${message}`;
-    chat.appendChild(newMessage);
-    newMessage.scrollIntoView({ behavior: "smooth", block: "end" });
-
-
-
-    // to create at sender side
-    let senderChat = document.getElementById(`chat-box-${user}`);
+    let senderChat = document.getElementById(`chat-box`);
     let senderMessage = document.createElement('div');
     senderMessage.classList.add('sender','message')
     senderMessage.textContent = `${message}`;
     senderChat.appendChild(senderMessage);
     senderMessage.scrollIntoView({ behavior: "smooth", block: "end" });
 
+    input.value = "";
+}
+
+socket.on("chat message", (message) => {
+
+    console.log("message recieved: " + message);
+
+    let chat = document.getElementById(`chat-box`);
+    let newMessage = document.createElement('div');
+    newMessage.classList.add('reciever','message')
+    newMessage.textContent = `${message}`;
+    chat.appendChild(newMessage);
+    newMessage.scrollIntoView({ behavior: "smooth", block: "end" });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "node:http";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,29 +11,33 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+  // for the ui where both users in same screen
 
-    socket.on('chat message', ({user , message}) => {
-        console.log('message: ' + user + ' ' + message);
-        io.emit('chat message', {user , message});
+  // socket.on("chat message", ({ user, message }) => {
+  //   console.log("message: " + user + " " + message);
+  //   io.emit("chat message", { user, message });
+  // });
 
-      });
 
-    
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-      })
+  socket.on("chat message", (msg) => {
+    console.log("message sended: " + msg);
+    socket.broadcast.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
-  
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
